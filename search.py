0abfb59c4e11fef9d192b61b83dec52c346b6108
Eastmary <tkdgns1592@naver.com>
2021-08-11 12:38:54 +0900

    문제 2번

---
+++ search.py
@@ -0,0 +1,25 @@
+import pymysql
+
+con, cur = None, None
+data1, data2, data3, data4 = "", "", "", ""
+row=None
+
+conn = pymysql.connect(host='127.0.0.1', user='root', password='1234', db='sqlDB', charset='utf8')
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM userTable")
+
+print("사용자ID    사용자이름     E메일    출생연도")
+print("----------------------------------------------------")
+
+while (True) :
+    row = cur.fetchone()
+    if row==None :
+        break
+    data1 = row[0]
+    data2 = row[1]
+    data3 = row[2]
+    data4 = row[3]
+    print("%10s   %10s   %15s   %5d" % (data1, data2, data3, data4))
+
+conn.close()
